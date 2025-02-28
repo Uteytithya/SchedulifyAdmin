aa8b36eb1212e45b9e8a3ffa1a125904b4a8e565
Uteytithya <tithyautey@gmail.com>
2025-02-28 10:59:04 +0700

    Fixed tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,6 +99,8 @@
                                             <p class="mt-4 text-sm/relaxed">
                                                 Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                             </p>
+                                            <div class="bg-blue-500 text-white p-4">If this is blue, Tailwind is working!  TESTING🎉</div>
+
                                         </div>
                                     </div>
 
