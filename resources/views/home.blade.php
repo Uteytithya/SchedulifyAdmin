694e503313ce40682dc84202787f8000443babb2
nangdypanhar <181545922+nangdypanhar@users.noreply.github.com>
2025-03-03 13:55:27 +0700

    close #5 create admin sidebar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+    <h1>This is home page</h1>
+@endsection
---
+++ resources/views/users.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="container mx-auto p-5">
+        <h1 class="text-3xl font-bold mb-4">Test Page</h1>
+        <p class="text-lg text-gray-700">This is a randomly generated test page to check your layout.</p>
+
+        <div class="mt-6">
+            <a href="/" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go Back Home</a>
+        </div>
+    </div>
+@endsection
