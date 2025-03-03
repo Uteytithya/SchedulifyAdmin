e1c33f3191a7da06605b39e5610a7e7f4b614f14
visalkorn22 <145747711+visalkorn22@users.noreply.github.com>
2025-03-03 15:48:25 +0700

    Finished Admin Navbar

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,8 @@
+<div class="header flex items-center justify-between p-4 bg-white shadow-md">
+    <div class="logo">
+        <img src="{{ asset('logo.png') }}" alt="Logo" width="240" height="54">
+    </div>
+    <div class="flec gap-8">
+        <img src="{{ asset('Tom-R.png') }}" width="40" height="40" alt="Profile" class="profile-image" />
+    </div>
+</div>
