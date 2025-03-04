bcc18920a9442714fb6630331fe839f09b8e0e6b
visalkorn22 <145747711+visalkorn22@users.noreply.github.com>
2025-03-04 15:11:41 +0700

    NavBar Finished

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,14 @@
-<div class="header flex items-center justify-between p-4 bg-white shadow-md">
+<div class="header flex items-center justify-between p-8 bg-white shadow-md">
     <div class="logo">
         <img src="{{ asset('logo.png') }}" alt="Logo" width="240" height="54">
     </div>
-    <div class="flec gap-8">
-        <img src="{{ asset('Tom-R.png') }}" width="40" height="40" alt="Profile" class="profile-image" />
+    <div class="flex gap-8 items-center">
+        <a href="/messages" class="text-gray-800 hover:text-gray-800 text-2xl">
+            <i class="fa-solid fa-envelope"></i>
+        </a>
+        <img src="{{ asset('Sad.jpg') }}" width="40" height="40" alt="Profile" class="profile-image rounded-full" />
+        <a href="/logout" class="text-gray-800 hover:text-gray-800 text-2xl">
+            <i class="fa-solid fa-sign-out-alt"></i>
+        </a>
     </div>
 </div>
