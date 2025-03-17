10f244f74e93acaeaaf05cce541c43cdac5846fe
Sengly456 <senglyou84@gmail.com>
2025-03-17 12:37:32 +0700

    refactor and fix course

--- resources/views/admin/course-view.blade.php
+++ resources/views/admin/course-view.blade.php
@@ -1,70 +1,66 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="container mx-6 px-4 py-6 mt-8 bg-white pr-6">
-       
+    <div class="container mx-auto px-4 py-6 max-w-7xl mt-20">
+        <span class="text-md text-gray-500 flex gap-1 ">
+            <p class="font-bold">Course ></p>
+            <p>List </p>
+        </span>
 
-        <div class="container mx-auto px-4 py-6 max-w-7xl mt-20">
-            <span class="text-md text-gray-500 flex gap-1 ">
-                <p class="font-bold">Course ></p>
-                <p>List </p>
-            </span>
+        <div class="flex justify-between items-center mb-6 mt-2">
+            <h1 class="text-3xl font-semibold">Course List</h1>
+            <a href="{{ route('admin.course_create') }}"
+                class="inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600 transition-colors">
+                Create Course
+            </a>
+        </div>
 
-            <div class="flex justify-between items-center mb-6 mt-2">
-                <h1 class="text-3xl font-semibold">Course List</h1>
-                <a href="{{ route('admin.course_create') }}" class="inline-block bg-blue-500 text-white px-6 py-2 rounded-lg shadow hover:bg-blue-600 transition-colors">
-                    Create User
-                </a>
-            </div>
-
-            {{-- search bar --}}
-            <div class="flex items-center ml-auto space-x-4  mb-4">
-                <form class="relative" action="" method="GET">
-                    <i class="fa-solid fa-magnifying-glass absolute left-2 top-3 text-gray-400 "></i>
-                    <input class="pl-10 bg-[#E9F5FE] p-2 rounded-lg w-64 mr-6" placeholder="Search" name="search">
-                </form>
-               
-            </div>
+        {{-- search bar --}}
+        <div class="flex items-center ml-auto space-x-4 mb-4">
+            <form class="relative flex items-center bg-[#E9F5FE] p-1 rounded-lg w-72 md:w-96 border-solid border-2">
+                <i class="fa-solid fa-magnifying-glass absolute left-3 text-gray-400"></i>
+                <input class="pl-10 pr-4 py-2 bg-transparent focus:outline-none w-full" placeholder="Search..." name="search">
+            </form>
+        </div>
+        
 
 
 
-            <div class="bg-white shadow-md rounded-lg overflow-hidden ">
-                <table class="min-w-full table-auto">
-                    <thead class="bg-gray-200">
-                        <tr class="border-b border-gray-300">
-                            <th class="px-4 py-3 text-left">Name</th>
-                            <th class="px-4 py-3 text-left">Credit</th>
-                            <th class="px-4 py-3 text-left">Actions</th>
+        <div class="bg-white shadow-md rounded-lg overflow-hidden ">
+            <table class="min-w-full table-auto">
+                <thead class="bg-gray-200">
+                    <tr class="border-b border-gray-300">
+                        <th class="px-4 py-3 text-left">Name</th>
+                        <th class="px-4 py-3 text-left">Credit</th>
+                        <th class="px-4 py-3 text-left">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if ($courses->isEmpty())
+                        <tr class="h-48">
+                            <td class="px-4 py-4 text-center" colspan="4">No course found</td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @if ($courses->isEmpty())
-                            <tr class="h-48">
-                                <td class="px-4 py-4 text-center" colspan="4">No course found</td>
-                            </tr>
-                        @endif
-                        @foreach ($courses as $course)
-                            <tr class="border-b border-gray-300">
-                                <td class="px-4 py-4">{{ $course->name }}</td>
-                                <td class="px-4 py-4">{{ $course->credit }}</td>
-                                <td class="px-4 py-4">
-                                    <a href="{{ route('admin.course_edit', $course->id) }}"
-                                        class="text-blue-500 hover:text-blue-700"><i
-                                            class="fa-solid fa-pen-to-square"></i></a>
-                                    <form action="{{ route('admin.course_delete_post', $course->id) }}" method="POST"
-                                        class="inline-block ml-2" id="delete-form-{{ $course->id }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="button" class="text-red-500 hover:text-red-700"
-                                            onclick="confirmDelete('{{ $course->id }}')">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+                    @endif
+                    @foreach ($courses as $course)
+                        <tr class="border-b border-gray-300">
+                            <td class="px-4 py-4">{{ $course->name }}</td>
+                            <td class="px-4 py-4">{{ $course->credit }}</td>
+                            <td class="px-4 py-4">
+                                <a href="{{ route('admin.course_edit', $course->id) }}"
+                                    class="text-blue-500 hover:text-blue-700"><i class="fa-solid fa-pen-to-square"></i></a>
+                                <form action="{{ route('admin.course_delete_post', $course->id) }}" method="POST"
+                                    class="inline-block ml-2" id="delete-form-{{ $course->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" class="text-red-500 hover:text-red-700"
+                                        onclick="confirmDelete('{{ $course->id }}')">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 @endsection
--- resources/views/admin/create-course.blade.php
+++ resources/views/admin/create-course.blade.php
@@ -1,14 +1,38 @@
 @extends('layouts.layout')
 @section('content')
-    <div class="container mx-6 px-4 py-6 mt-8 bg-white flex-col">
-        <h2 class="text-2xl font-extrabold mb-6">Create Course</h2>
-        <form class="" action="{{ route('admin.course_create_post') }}" method="POST">
-            @csrf
-            <label class="font-bold block">Name</label>
-            <input class="border-solid border-2 py-2 pl-1 rounded-sm mb-4" name="name" placeholder="Name"><br>
-            <label class="font-bold block">Credit</label>
-            <input class="border-solid border-2 py-2 pl-1 rounded-sm mb-4 block" name="credit" placeholder="Credit">
-            <button class="bg-[#284BAD] text-white py-1 px-3 rounded-lg">Create</button>
+    <div class="container mx-auto px-4 py-6 max-w-7xl mt-20">
+        <span class="text-md text-gray-500 flex gap-1 ">
+            <a class="font-bold" href="{{ route('admin.course') }}">Course ></a>
+            <p>Create </p>
+        </span>
+        <h1 class="text-3xl font-semibold mt-2">Create Course</h1>
+
+        <div class= "mt-8 ">
+            <form action="{{ route('admin.course_create_post') }}" method="POST">
+                @csrf
+                <div class="grid grid-cols-2 gap-x-6 gap-y-4 bg-white shadow-sm p-6 rounded-lg">
+                    <div class="mb-5">
+                        <label for="name" class="block mb-2 text-md font-medium text-gray-900">Name :</label>
+                        <input type="text" id="name" name="name" placeholder="name"
+                            class="p-2 w-full rounded-lg border border-gray-300 focus:border-2 focus:border-blue-500 outline-none" />
+                        @error('name')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-5">
+                        <label  class="block mb-2 text-md font-medium text-gray-900">Credit :</label>
+                        <input type="text" name="credit" id="credit" placeholder="credit"
+                            class="p-2 w-full rounded-lg border border-gray-300 focus:border-2 focus:border-blue-500 outline-none" />
+                        @error('credit')
+                            <span class="text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+        </div>
+        <button type="submit"
+            class="focus:outline-none text-white bg-green-600 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 mt-5 ">Create</button>
         </form>
+    </div>
     </div>
 @endsection
